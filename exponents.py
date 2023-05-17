651ac96812563ec0aada1a1eb8d8deba0b7e2682
Vanica Lobusta <132590418+Akinab@users.noreply.github.com>
2023-05-17 12:06:02 +0800

    Interation initialization.

--- exponents.py
+++ exponents.py
@@ -9,3 +9,5 @@ details ()
 with open('integers.txt', 'r') as source_file:
     # Create two new files for writing
     with open('double.txt', 'w') as double_file, open('triple.txt', 'w') as triple_file:
+        # Iterate over each line in the source file
+        
